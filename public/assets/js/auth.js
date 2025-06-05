a81a255169a62b04a75025cd733dc0521d20ab89
Sayan-dev731 <sayancodder731@gmail.com>
2025-06-06 03:18:52 +0530

    serverURL fixed

--- public/assets/js/auth.js
+++ public/assets/js/auth.js
@@ -1,6 +1,6 @@
 // const serverUrl = 'https://devsync-opensource.tech';
-// const serverUrl = 'https://www.devsync.club';
-const serverUrl = 'http://localhost:3000';
+const serverUrl = 'https://www.devsync.club';
+// const serverUrl = 'http://localhost:3000';
 
 async function checkAuthStatus() {
     try {
